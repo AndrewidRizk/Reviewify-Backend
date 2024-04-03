f8003ff3df00d55ad0351f4b9e3c9c9992c9233a
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-04-03 14:38:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template, Response
 from flask_cors import CORS
-import time
+from places import main_get_total_reviews
+from cohere_api import classify_reviews, summarize_reviews
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -12,42 +13,53 @@ messages.append(message)
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
 
 @app.route('/stream')
 def stream():
     def generate_messages():
-        while True:
-            if messages:
-                yield f"{messages[-1]}\n\n"
-                messages.clear()  # Clear the messages after sending
-            else:
-                time.sleep(1)  # Wait for 1 second before checking again
-
+        for message in messages:
+            yield f"{message}\n\n"
     return Response(generate_messages(), content_type='text/event-stream')
 
 @app.route('/flask/process_data', methods=['POST'])
 def process_data():
+    global messages
+    message = "🔌 Attempting to Connect to the Backend"
+    messages.append(message)
     data = request.json  # Access JSON data sent from the form
+    # Perform further processing if needed
+    # Example: Extracting specific fields from the data
     restaurant = data.get('restaurant', '')  # getting the restaurant name
     postal = data.get('postal', '')  # getting the restaurant postal code
     if restaurant and postal:
         message = f"📬 Receiving Restaurant name: {restaurant} and Postal code: {postal}"
         messages.append(message)
-        # Simulate processing time
-        time.sleep(3)
-        message = "💻 Retrieving all Reviews"
-        messages.append(message)
-        # Simulate processing time
-        time.sleep(2)
-        message = "📝 Summarizing the Reviews"
-        messages.append(message)
-        # Simulate processing time
-        time.sleep(1)
-        message = "📨 Sending the information back"
-        messages.append(message)
 
-    return jsonify({"status": "Processing data..."})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    reviews_from_places = main_get_total_reviews(restaurant_name=restaurant, postal_code=postal)  # getting all the reviews
+    message = "💻 Retrieving all Review "
+    messages.append(message)
+    # Calling the cohere functions
+    # returning the classified results inputting the reviews and getting positive, negative, and unrelated
+    classified_result, pos, neg, unrel = classify_reviews(reviews_from_places)
+    # summarize the code
+    message = "📝 Summarizing the Reviews"
+    messages.append(message)
+    try:
+        summary = summarize_reviews(classified_result)
+    except Exception as e:
+        message = f"❌ Error: {e}"
+        messages.append(message)
+    # Calculate percentages
+    positive = (pos / 5) * 100
+    negative = (neg / 5) * 100
+    unrelated = (unrel / 5) * 100
+    # Construct response data
+    response_data = {
+        "resultStatus": "Success",
+        'message': [positive, negative, unrelated, summary[0], summary[1]]
+    }
+    # Return processed data
+    message = "📨 Sending the information back"
+    messages.append(message)
+    return jsonify(response_data)
