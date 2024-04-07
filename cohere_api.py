0221d75e908153619d2638c76e8aabee194c8daa
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-04-07 14:44:29 -0400

    Update cohere_api.py

--- cohere_api.py
+++ cohere_api.py
@@ -68,12 +68,12 @@ def summarize_reviews(classified_reviews):
   for i in classified_reviews:
     if (i[1] == 'positive'):
       if len(i[0]) > 420:
-         positive_text += positive_text + " " + i[0][:420]
+         positive_text += positive_text + " " + i[0][:400]
       else:
         positive_text += positive_text + " " + i[0]
     elif (i[1] == 'negative'):
       if len(i[0]) > 420:
-         negative_text += negative_text + " " + i[0][:420]
+         negative_text += negative_text + " " + i[0][:400]
       else:
         negative_text += negative_text + " " + i[0]
 
